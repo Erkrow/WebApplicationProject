136b7ffeb04c859e83cd47eb14fe26b2da63907a
jumidu <michal24d@op.pl>
2026-05-25 01:29:20 +0200

    Gitary_baza i zdjecie przykladowe
    Trzeba bedzie zmienic na dyanmiczne jak najlatwiej dodawanie tych zdjec, na razie pogladowo i bedzie sie dodawac te produkty

--- musicshop-frontend/src/components/ui/ProductCard.tsx
+++ musicshop-frontend/src/components/ui/ProductCard.tsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import type { Product } from '../../types';
 import { CartContext } from '../../context/CartContext';
-
+import elektryczkaImg from '../../assets/elektryczka.jpg';
 interface ProductCardProps {
     product: Product;
 }
@@ -36,10 +36,12 @@ export default function ProductCard({ product }: ProductCardProps) {
                     <button className="pcard-wishlist-btn">
                         <svg width="16" height="16" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 21.35L10.55 20.03C5.4 15.36 2 12.28 2 8.5C2 5.42 4.42 3 7.5 3C9.24 3 10.91 3.81 12 5.09C13.09 3.81 14.76 3 16.5 3C19.58 3 22 5.42 22 8.5C22 12.28 18.6 15.36 13.45 20.04L12 21.35Z" stroke="#a89080" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/></svg>
                     </button>
+
                     {product.image ? (
                         <img src={product.image} alt={product.name} />
                     ) : (
-                        <svg width="64" height="64" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 18.25C10 18.25 10 18.9424 10.375 19.3125C10.75 19.6826 11.3375 19.75 11.3375 19.75H12.65C12.65 19.75 13.2375 19.6826 13.6125 19.3125C13.9875 18.9424 14 18.25 14 18.25V17.75H10V18.25Z" stroke="#b45309" stroke-width="1.2"/><path d="M12 2.5V3.5" stroke="#b45309" stroke-width="1.2"/><path d="M15.5 12C15.9125 11.9158 16.31 11.7519 16.68 11.515C17.775 10.825 18.5 9.6125 18.5 8.25C18.5 5.7625 16.5 4.5 12 4.5C7.5 4.5 5.5 5.7625 5.5 8.25C5.5 9.6125 6.225 10.825 7.32 11.515C7.68997 11.7519 8.08751 11.9158 8.5 12H15.5Z" stroke="#b45309" stroke-width="1.2"/><path d="M8.5 12V17.75H15.5V12" stroke="#b45309" stroke-width="1.2"/></svg>
+                        // <svg width="64" height="64" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 18.25C10 18.25 10 18.9424 10.375 19.3125C10.75 19.6826 11.3375 19.75 11.3375 19.75H12.65C12.65 19.75 13.2375 19.6826 13.6125 19.3125C13.9875 18.9424 14 18.25 14 18.25V17.75H10V18.25Z" stroke="#b45309" stroke-width="1.2"/><path d="M12 2.5V3.5" stroke="#b45309" stroke-width="1.2"/><path d="M15.5 12C15.9125 11.9158 16.31 11.7519 16.68 11.515C17.775 10.825 18.5 9.6125 18.5 8.25C18.5 5.7625 16.5 4.5 12 4.5C7.5 4.5 5.5 5.7625 5.5 8.25C5.5 9.6125 6.225 10.825 7.32 11.515C7.68997 11.7519 8.08751 11.9158 8.5 12H15.5Z" stroke="#b45309" stroke-width="1.2"/><path d="M8.5 12V17.75H15.5V12" stroke="#b45309" stroke-width="1.2"/></svg>
+                        <img src={elektryczkaImg} alt={product.name} />
                     )}
                 </div>
                 <div className="pcard-body">
